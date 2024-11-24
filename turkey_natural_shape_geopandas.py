23693ccba1a9a13961bae554909c1e2a03ff34d0
dilamutlu <dilamutlu81@gmail.com>
2024-11-24 18:57:21 +0300

    Proje son haline getirildi

--- turkey_natural_shape_geopandas.py
+++ turkey_natural_shape_geopandas.py
@@ -2,8 +2,19 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-gdf = gpd.read_file("dila-mutlu-mid-term-exam-2/turkey-points-shape/points.shp")
-print(gdf.head())
+df = gpd.read_file("dila-mutlu-mid-term-exam-2/turkey-points-shape/points.shp")
+print(df.head())
+
+# geometry sütunundan koordinatları ayıkla (nokta geometriler için)
+df['longitude'] = df.geometry.x
+df['latitude'] = df.geometry.y
+
+# Ayrıştırılmış DataFrame
+xdf = df[['osm_id', 'name', 'type', 'longitude', 'latitude']]
+
+gdf = gpd.GeoDataFrame(
+    xdf, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326"
+)
 
 world = gpd.read_file("dila-mutlu-mid-term-exam-2/ne_110m_land/ne_110m_land.shp")
 
